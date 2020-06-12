c14e380064668879852db9caf9b394e3d7eae11c
EroMCakes <44838692+EroMCakes@users.noreply.github.com>
2020-06-12 09:19:49 +0100

    HPS FR/EN for iOs migration

---
+++ HPS/HPS/Assets/Scripts/HPS_Code.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPS_Code : MonoBehaviour
+{
+    public InputField Pass;
+    public string code = null;
+    public Text txt;
+    public string To_Load1;
+    public string To_Load2;
+    public string To_Load3;
+    public string To_Load4;
+    public string To_Load5;
+    public string To_Load6;
+    public string To_Load7;
+    int wordIndex = 0;
+    [SerializeField] public Text Aff_alpha = null;
+
+
+
+    public void Enter_Push()
+    {
+        code = Pass.text;
+        if (Pass.text != null)
+        {
+            if (code == "HPS")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load1);
+            }
+            else if (code == "1255")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load2);
+            }
+            else if (code == "SECURE")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load3);
+            }
+            else if (code == "750")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load4);
+            }
+            else if (code == "SIMPLE")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load5);
+            }
+            else if (code == "773485")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load6);
+            }
+            else if (code == "FEEL GOOD ABOUT PAYMENTS")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load7);
+            }
+            else
+            {
+                txt.text = "Code erroné !";
+                Pass.text = null;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
